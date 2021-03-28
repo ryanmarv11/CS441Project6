df6269e521b62861e6a1ee8d883d25dd247a64f1
ryanmarv11 <30666230+ryanmarv11@users.noreply.github.com>
2021-03-27 23:42:45 -0400

    Made a screen, more updates to come

--- app/src/main/java/com/example/cs441project6/MainActivity.java
+++ app/src/main/java/com/example/cs441project6/MainActivity.java
@@ -6,12 +6,14 @@ import java.lang.reflect.Array;
 import java.io.*;
 import java.util.ArrayList;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         ArrayList<String> nameList = new ArrayList<>();
         nameList = createNameList();
