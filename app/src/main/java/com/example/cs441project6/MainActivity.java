9de48613fc4437a8c2d971ded86facd94309e16f
ryanmarv11 <30666230+ryanmarv11@users.noreply.github.com>
2021-04-07 02:56:39 -0400

    Splash Screen Complete

--- app/src/main/java/com/example/cs441project6/MainActivity.java
+++ app/src/main/java/com/example/cs441project6/MainActivity.java
@@ -5,19 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.lang.reflect.Array;
 import java.io.*;
 import java.util.ArrayList;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 2000; //2 seconds
+
+
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.magicCardBack);
+        message = findViewById(R.id.textView);
+
+        image.setAnimation(topAnim);
+        message.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
         ArrayList<String> nameList = new ArrayList<>();
         nameList = createNameList();
         ArrayList<MTGSet> setList = createSetList(nameList);
+
 
 
     }
